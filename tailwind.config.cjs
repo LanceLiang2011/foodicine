a8e6f7754f1e9486c1abb602b726a5446b6d8f19
Lance Liang <liangxianyi2011@gmail.com>
2023-07-28 21:56:08 -0400

    Install astro icon

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,24 @@ module.exports = {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-1": "#034e7b",
+        "primary-2": "#255c8c",
+        "secondary-blue-light": "#4be4ff",
+        "secondary-cyan": "#3edad8",
+        "secondary-blue-md": "#38b6ff",
+        "secondary-blue-pale": "#5097d7",
+        "secondary-blue-dark": "#041f60",
+        "secondary-gray": "#c8dbe0",
+      },
+      fontFamily: {
+        serif: ["Open Sans", "sans-serif"],
+      },
+      backgroundImage: {
+        "hero-image": "url('/hero.png')",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
