afa8dd2267597ebd59dffbacc0950a57ef4ed808
Lance Liang <liangxianyi2011@gmail.com>
2023-08-20 22:13:03 -0400

    init project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,20 +7,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "primary-1": "#034e7b",
-        "primary-2": "#255c8c",
-        "secondary-blue-light": "#4be4ff",
-        "secondary-cyan": "#3edad8",
-        "secondary-blue-md": "#38b6ff",
-        "secondary-blue-pale": "#5097d7",
-        "secondary-blue-dark": "#041f60",
-        "secondary-gray": "#c8dbe0",
+
       },
       fontFamily: {
         serif: ["Open Sans", "sans-serif"],
       },
       backgroundImage: {
-        "hero-image": "url('/hero.png')",
+        "hero-image": "url('/hero.jpg')",
       },
     },
   },
